43c86eded5b59cc7250b9a792c649fd7a0d71a00
tedtalktimmy <frtntn007@gmail.com>
2023-05-05 21:21:35 -0400

    added properties to panel

--- GameFrame.java
+++ GameFrame.java
@@ -8,6 +8,15 @@ public class GameFrame extends JFrame {
   GamePanel panel = new GamePanel();
 
   GameFrame() {
+    panel = new GamePanel();
+    this.add(panel);
+    this.setTitle("Pong Game");
+    this.setResizable(false);
+    this.setBackground(Color.black);
+    this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    this.pack();
+    this.setVisible(true);
+    this.setLocationRelativeTo(null);
 
   }
   
